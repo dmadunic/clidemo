9f89c9c6c02df486d94597cc99a76793c7312005
Domagoj Madunić <domagoj.madunic@gmail.com>
2019-03-17 13:43:04 +0100

    Configured InputReader with its own Linereader

--- src/main/java/com/ag04/clidemo/config/SpringShellConfig.java
+++ src/main/java/com/ag04/clidemo/config/SpringShellConfig.java
@@ -3,6 +3,7 @@ package com.ag04.clidemo.config;
 import com.ag04.clidemo.shell.InputReader;
 import com.ag04.clidemo.shell.PromptColor;
 import com.ag04.clidemo.shell.ShellHelper;
+import org.jline.reader.History;
 import org.jline.reader.LineReader;
 import org.jline.reader.LineReaderBuilder;
 import org.jline.reader.Parser;
@@ -12,7 +13,7 @@ import org.jline.utils.AttributedStyle;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
-
+import org.springframework.shell.jline.JLineShellAutoConfiguration;
 
 @Configuration
 public class SpringShellConfig {
@@ -23,14 +24,18 @@ public class SpringShellConfig {
     }
 
     @Bean
-    public InputReader inputReader(@Lazy LineReader lineReader) {
-        return new InputReader(lineReader, this.shellHelper(lineReader));
-    }
-
-    /*
-    @Bean
-    public InputReader inputReader(@Lazy Terminal terminal, @Lazy Parser parser) {
-        LineReaderBuilder lineReaderBuilder = LineReaderBuilder.builder().terminal(terminal).highlighter(
+    public InputReader inputReader(
+            @Lazy Terminal terminal,
+            @Lazy Parser parser,
+            JLineShellAutoConfiguration.CompleterAdapter completer,
+            @Lazy History history,
+            ShellHelper shellHelper
+    ) {
+        LineReaderBuilder lineReaderBuilder = LineReaderBuilder.builder()
+            .terminal(terminal)
+            .completer(completer)
+            .history(history)
+            .highlighter(
             (LineReader reader, String buffer) -> {
                 return new AttributedString(
                     buffer, AttributedStyle.BOLD.foreground(PromptColor.WHITE.toJlineAttributedStyle())
@@ -40,7 +45,6 @@ public class SpringShellConfig {
 
         LineReader lineReader = lineReaderBuilder.build();
         lineReader.unsetOpt(LineReader.Option.INSERT_TAB);
-        return new InputReader(lineReader);
-    }*/
-
+        return new InputReader(lineReader, shellHelper);
+    }
 }
