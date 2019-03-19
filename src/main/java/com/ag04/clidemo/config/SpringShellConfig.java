1e83ce23975e1d625f71147d886f5ea181f627d1
Domagoj Madunić <domagoj.madunic@gmail.com>
2019-03-19 22:21:00 +0100

    Modified ShellHelper to work with Terminal instead of LineReader

--- src/main/java/com/ag04/clidemo/config/SpringShellConfig.java
+++ src/main/java/com/ag04/clidemo/config/SpringShellConfig.java
@@ -1,7 +1,7 @@
 package com.ag04.clidemo.config;
 
 import com.ag04.clidemo.shell.ShellHelper;
-import org.jline.reader.LineReader;
+import org.jline.terminal.Terminal;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
@@ -10,8 +10,8 @@ import org.springframework.context.annotation.Lazy;
 public class SpringShellConfig {
 
     @Bean
-    public ShellHelper shellHelper(@Lazy LineReader lineReader) {
-            return new ShellHelper(lineReader);
+    public ShellHelper shellHelper(@Lazy Terminal terminal) {
+            return new ShellHelper(terminal);
     }
 
 }
