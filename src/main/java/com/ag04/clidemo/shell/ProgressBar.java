71379bed705cc648943992f20e23af583cdac93e
Domagoj Madunic <domagoj.madunic@gmail.com>
2019-03-26 15:54:41 +0100

    Expanded ProgressBar with new display() method that also has statusMessage parameter.

--- src/main/java/com/ag04/clidemo/shell/ProgressBar.java
+++ src/main/java/com/ag04/clidemo/shell/ProgressBar.java
@@ -15,15 +15,20 @@ public class ProgressBar {
     }
 
     public void display(int percentage) {
+        display(percentage, null);
+    }
+
+    public void display(int percentage, String statusMessage) {
         int x = (percentage/5);
         int y = 20-x;
+        String message = ((statusMessage == null) ? "" : statusMessage);
 
         String done = shellHelper.getSuccessMessage(new String(new char[x]).replace("\0", doneMarker));
         String remains = new String(new char[y]).replace("\0", remainsMarker);
 
         String progressBar = String.format("%s%s%s%s %d", leftDelimiter, done, remains, rightDelimiter, percentage);
 
-        shellHelper.getTerminal().writer().print("\r" + progressBar + "%");
+        shellHelper.getTerminal().writer().print("\r" + progressBar + "% " + message);
         shellHelper.getTerminal().flush();
     }
 
